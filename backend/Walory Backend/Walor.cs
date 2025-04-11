300e7b7d8001f390fbce871459a41a77e4ceda80
Kacper <74537153+KasmyrA@users.noreply.github.com>
2025-04-11 16:53:23 +0200

    Added walor

---
+++ backend/Walory Backend/Walor.cs
@@ -0,0 +1,13 @@
+namespace Walory_Backend
+{
+    public class Walor
+    {
+        public int Id { get; set; } 
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
