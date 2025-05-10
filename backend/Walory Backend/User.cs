feba6cf263232cdb83aba6e6586de3310ceb4aff
Norbert <norbertluchow@gmail.com>
2025-05-10 10:48:20 +0200

    User Entity fixing compilation error lacking ;

--- backend/Walory Backend/User.cs
+++ backend/Walory Backend/User.cs
@@ -3,7 +3,7 @@ namespace Walory_Backend
 {
     public class User
     {
-        public int Id { get;set }
+        public int Id { get; set; }
         public string Name { get;set; }
         public string Email { get;set; }
         public string Password { get; set; }
