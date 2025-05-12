f1f3cf1071d874b5120eb93bf4b3394c6b622b33
Norbert <norbertluchow@gmail.com>
2025-05-12 20:21:39 +0200

    Deleting copies of entityies

--- backend/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ backend/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b5b582626ff035e304c0040a05b9daa0ce8db34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7d21cad53575e133580fa17be06e093df39ac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
