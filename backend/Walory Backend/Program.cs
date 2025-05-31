ceff32aaef3c9a0bd720a8798f2a1ea30575450b
Kacper <74537153+KasmyrA@users.noreply.github.com>
2025-05-31 19:53:36 +0200

    Update Program.cs

--- backend/Walory Backend/Program.cs
+++ backend/Walory Backend/Program.cs
@@ -86,11 +86,11 @@ builder.Services.AddScoped<EmailService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment()) //Yes I want swager on production right now
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors("AllowFrontend");
 
