b2ccb765d3fe60a1dc84144a55d1e4e900632085
Kacper <74537153+KasmyrA@users.noreply.github.com>
2025-06-17 21:36:00 +0200

    Update main.ts
    nic nie powinienem zepsuć ale zawsze można się przekonać

--- frontend/walory-portal/src/main.ts
+++ frontend/walory-portal/src/main.ts
@@ -3,6 +3,10 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+const API_BASE_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:8080'
+  : `http://${window.location.hostname}:8080`;
+window.API_BASE_URL = API_BASE_URL;
 
 const app = createApp(App)
 app.use(router)
