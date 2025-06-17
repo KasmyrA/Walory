431863cefbeb78935edf5f7785fe8de156e4cf38
coolm <coolman02nl@gmail.com>
2025-06-17 10:45:26 +0200

    adding cors

--- backend/Walory Backend/Program.cs
+++ backend/Walory Backend/Program.cs
@@ -43,7 +43,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost", "http://localhost:80", "http://localhost:5173")
+            policy.WithOrigins("http://localhost", "http://localhost:80", "http://localhost:5173", "http://20.67.251.224/")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
